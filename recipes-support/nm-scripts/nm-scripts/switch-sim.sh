d878453d0e85a11b5e079fa472311ecf0a62ca43
Stefan Eichenberger <stefan.eichenberger@netmodule.com>
2017-07-21 15:10:35 +0200

    switch-sim: do some cleanup

--- recipes-support/nm-scripts/nm-scripts/switch-sim.sh
+++ recipes-support/nm-scripts/nm-scripts/switch-sim.sh
@@ -10,23 +10,24 @@ usage()
     echo "  fpga: "
     echo "        0 means FPGA on hw14/08"
     echo "        1 means FPGA on hw17/hw18"
-    echo "  sim: simcard slot starting with 1"
-    echo "  slot: pcie slot to connect sim to. Starting with 1, 0 means disable"
+    echo "  sim: SIM card slot starting with 1"
+    echo "  slot: PCIe slot where the SIM should be connected to. Starting with 1, 0 means disable"
+    echo
 }
 
 if [ "$hw" = "" ]; then
     usage
-    return 1
+    exit 1
 fi
 
 if [ "$sim" = "" ]; then
     usage
-    return 1
+    exit 1
 fi
 
 if [ "$slot" = "" ]; then
     usage
-    return 1
+    exit 1
 fi
 
 switch_fpga_0()
@@ -36,16 +37,16 @@ switch_fpga_0()
 
     if [ $sim -lt 1 -o $sim -gt 4 ]; then
         echo "SIM should be between 1 and 4"
-        return 2
+        exit 2
     fi
 
     if [ $slot -lt 0 -o $slot -gt 6 ]; then
         echo "SLOT should be between 0 and 4"
-        return 3
+        exit 3
     fi
 
+    # Use incredible simbus function
     simbus_path="/sys/class/leds/simbus"
-    # set bitwise
     case $slot in
         1)
             echo 1 > $simbus_path"13:en/brightness"
@@ -69,19 +70,20 @@ switch_fpga_0()
             ;;
     esac
 
+    # On HW08/HW14 we need to enable/disable sim slot
     sim_path="/sys/class/leds/sim$sim"
     if [ $sim -eq 0 ]; then
         echo 0 > "$sim_path:en"
     fi
 
-    # set bitwise
+    # Now set sim sel according to slot selection
     sim_path="/sys/class/leds/sim$sim:sel"
+    # Because we enable/disable sim, 0 already means sim 1 (starting at 1)
     slot=$(($slot - 1))
     for i in $(seq 0 1); do
-        # inverse logic test returns 0 on success
+        # inverse logic test exits 0 on success
         test 0 -eq $(($slot & (2**$i)))
         val=$?
-        echo $slot: $i: $val
         echo $val > "$sim_path$i/brightness"
     done
 }
@@ -93,18 +95,18 @@ switch_fpga_1()
 
     if [ $sim -lt 1 -o $sim -gt 4 ]; then
         echo "SIM should be between 1 and 4"
-        return 2
+        exit 2
     fi
 
     if [ $slot -lt 0 -o $slot -gt 4 ]; then
         echo "SLOT should be between 0 and 4"
-        return 3
+        exit 3
     fi
 
-    # set bitwise
+    # Set sim sel according to slot
     sim_path="/sys/class/leds/sim$sim:sel"
     for i in $(seq 0 2); do
-        # inverse logic test returns 0 on success
+        # inverse logic test exits 0 on success
         test 0 -eq $(($slot & (2**$i)))
         val=$?
         echo $val > "$sim_path$i/brightness"
@@ -113,12 +115,14 @@ switch_fpga_1()
 
 case $hw in
     0)
-        switch_fpga_0 $sim $slot
+        exit switch_fpga_0 $sim $slot
         ;;
     1)
-        switch_fpga_1 $sim $slot
+        exit switch_fpga_1 $sim $slot
         ;;
     *)
         usage
         ;;
 esac
+
+exit 0
